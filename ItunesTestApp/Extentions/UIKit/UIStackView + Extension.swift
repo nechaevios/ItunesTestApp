4251a3876d8be99bd1ecf5a2912d41d90a51505b
Sergey <21218903+xchakiex@users.noreply.github.com>
2022-01-10 16:08:49 +0300

    extension added

---
+++ ItunesTestApp/Extentions/UIKit/UIStackView + Extension.swift
@@ -0,0 +1,18 @@
+//
+//  UIStackView + Extension.swift
+//  ItunesTestApp
+//
+//  Created by Nechaev Sergey  on 10.01.2022.
+//
+
+import UIKit
+
+extension UIStackView {
+    convenience init(arrangedSubviews: [UIView], axis: NSLayoutConstraint.Axis, spacing: CGFloat, distribution: UIStackView.Distribution) {
+        self.init(arrangedSubviews: arrangedSubviews)
+        self.axis = axis
+        self.spacing = spacing
+        self.distribution = distribution
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
