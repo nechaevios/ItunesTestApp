f418fb877905ccd798e727d2ff2d7ca0a53d8710
Sergey <21218903+xchakiex@users.noreply.github.com>
2022-01-12 19:27:44 +0300

    Add alertVC

---
+++ ItunesTestApp/Models/AlertOk.swift
@@ -0,0 +1,19 @@
+//
+//  AlertOk.swift
+//  ItunesTestApp
+//
+//  Created by Nechaev Sergey  on 12.01.2022.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func alertOk(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let ok = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(ok)
+        present(alert, animated: true, completion: nil)
+    }
+}
