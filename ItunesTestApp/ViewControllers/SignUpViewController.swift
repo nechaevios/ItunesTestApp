57730775164ccab957ff5581452433df875c2714
Sergey <21218903+xchakiex@users.noreply.github.com>
2022-01-13 15:07:13 +0300

    spacing cleanup

--- ItunesTestApp/ViewControllers/SignUpViewController.swift
+++ ItunesTestApp/ViewControllers/SignUpViewController.swift
@@ -139,9 +139,7 @@ class SignUpViewController: UIViewController {
         setConstraints()
         setupDelegate()
         setupDataPicker()
-        
         registerKeyboardNotification()
-        
     }
     
     deinit {
